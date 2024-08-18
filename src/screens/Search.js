cb44d4404fd66a9addb4d4605243bd07969594be
Lucas Carvalho Silva <lucascarvalhosilva2901@gmail.com>
2024-08-17 21:23:28 -0300

    feat: firestore create answer

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -2,22 +2,45 @@ import { View ,StyleSheet, ScrollView } from "react-native"
 import { Title } from "../components/Title"
 import { FeedbackOption } from "../components/FeedbackOption"
 
-export function Search(props) {
+import { initializeFirestore, collection, addDoc } from "firebase/firestore"
+import { app } from "../firebase/config"
 
-    function goToAcknowledgment() {
-        props.navigation.navigate("Acknowledgment")
+import { useSelector } from 'react-redux'
+
+export function Search(props) {
+    const db = initializeFirestore(app, {experimentalForceLongPolling: true})
+    const answerCollection = collection(db, "answer")
+
+    const searchID = useSelector((state) => state.search.id)
+
+    function answerSearch(answer) {
+        const answerDoc = {
+            searchID: searchID,
+            answer: answer
+        }
+        
+        addDoc(answerCollection, answerDoc)
+        .then((docRef) => {
+            console.log("Resposta Salva com sucesso ==> ", docRef)
+            props.navigation.navigate('Acknowledgment'); 
+        })
+        .catch((error) => {
+            console.error("Erro ao salvar documento", error)
+        })
+
     }
+    
 
     return (
         <View style={estilo.container}>
             <View style={estilo.content}>
                 <Title title="O que você achou do Carnaval 2024?"/>
                 <ScrollView horizontal={true} style>
-                    <FeedbackOption name="sentiment-very-dissatisfied" color="#D71616" feedback="Péssimo" action={goToAcknowledgment}/>
-                    <FeedbackOption name="sentiment-dissatisfied" color="#FF360A" feedback="Péssimo" action={goToAcknowledgment}/>
-                    <FeedbackOption name="sentiment-neutral" color="#FFC632" feedback="Péssimo" action={goToAcknowledgment}/>
-                    <FeedbackOption name="sentiment-satisfied" color="#37BD6D" feedback="Péssimo" action={goToAcknowledgment}/>
-                    <FeedbackOption name="sentiment-very-satisfied" color="#25BC22" feedback="Péssimo" action={goToAcknowledgment}/>
+                    <FeedbackOption name="sentiment-very-dissatisfied" color="#D71616" feedback="Péssimo" action={() => answerSearch(1)}/>
+                    <FeedbackOption name="sentiment-dissatisfied" color="#FF360A" feedback="Péssimo" action={() => answerSearch(2)}/>
+                    <FeedbackOption name="sentiment-neutral" color="#FFC632" feedback="Péssimo" action={() => answerSearch(3)}/>
+                    <FeedbackOption name="sentiment-satisfied" color="#37BD6D" feedback="Péssimo" action={() => answerSearch(4)}/>
+                    <FeedbackOption name="sentiment-very-satisfied" color="#25BC22" feedback="Péssimo" action={() => answerSearch(5)}/>
                 </ScrollView>
             </View>      
         </View>
